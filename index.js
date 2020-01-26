c3410a783c2e3992961989527e689edba2195a1f
Jason Brando <jasonbrandoo@gmail.com>
2020-01-27 00:58:39 +0700

    Clean up ignored files

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-let input = document.getElementById("input");
-let input2 = document.getElementById("input2");
-let isi = document.getElementById("apa");
-let button = document.getElementById("button");
-let del = document.getElementById("delete");
-let arr = [];
-del.addEventListener("click", function() {
-  arr = [];
-  isi.innerHTML = "";
-});
-button.addEventListener("click", function() {
-  let first_name = input.value;
-  let last_name = input2.value;
-  let full_name = {
-    nama_depan: first_name,
-    nama_belakang: last_name
+const firstName = document.querySelector("[name=firstName]");
+const lastName = document.querySelector("[name=lastName]");
+const submit = document.querySelector("#addNewPerson");
+
+submit.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const newPerson = {
+    firstName: firstName.value,
+    lastName: lastName.value
   };
-  console.log(full_name);
-  arr.push(full_name);
-  console.log(arr);
-  // return [...arr,full_name]
-  isi.innerHTML = "";
-  arr.forEach(function(object) {
-    isi.innerHTML += `
-  <tr>
-  <td>${object.nama_depan}</td>
-  <td>${object.nama_belakang}</td>
-  <td><button id='Delete'>Delete</button></td>
-  </tr>
-  `;
-  });
+  console.log(newPerson);
 });
