c80af8a2d195137f3e7f005b6fb432617f8eea7c
Jason Brando <jasonbrandoo@gmail.com>
2020-01-27 19:50:12 +0700

    Dikit lagi lur

---
+++ data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    firstName: "Jason",
+    lastName: "Brando"
+  },
+  {
+    id: 2,
+    firstName: "Noviar Putra",
+    lastName: "Budiawan"
+  },
+  {
+    id: 3,
+    firstName: "Kemas Jaka",
+    lastName: "Samudra"
+  },
+  {
+    id: 4,
+    firstName: "Abiyyu Jundi",
+    lastName: "Hidayat"
+  }
+];
--- index.js
+++ index.js
@@ -1,12 +1,57 @@
+import data from "./data.js";
+
+if (!localStorage.getItem("person")) {
+  localStorage.setItem("person", JSON.stringify(data));
+}
+
+let person = JSON.parse(localStorage.getItem("person"));
 const firstName = document.querySelector("[name=firstName]");
 const lastName = document.querySelector("[name=lastName]");
 const submit = document.querySelector("#addNewPerson");
+const list = document.querySelector("#listPerson");
+
+person.forEach(function(i) {
+  list.innerHTML += `
+  <tr>
+    <td>${i.firstName}</td>
+    <td>${i.lastName}</td>
+      <td><button type="button" class="deleteButton" value="${i.id}">Delete</button></td>
+    </td>
+  `;
+});
 
 submit.addEventListener("submit", function(e) {
   e.preventDefault();
   const newPerson = {
+    id: person.length + 1,
     firstName: firstName.value,
     lastName: lastName.value
   };
-  console.log(newPerson);
+  person.push(newPerson);
+  localStorage.setItem("person", JSON.stringify(person));
+
+  list.innerHTML = "";
+  person.forEach(function(i) {
+    list.innerHTML += `
+    <tr>
+      <td>${i.firstName}</td>
+      <td>${i.lastName}</td>
+      <td><button type="button" class="deleteButton" value="${i.id}">Delete</button></td>
+    </td>
+  `;
+  });
+});
+
+list.addEventListener("click", function(e) {
+  if (e.target.className === "deleteButton") {
+    console.log(e);
+    const td = e.target.parentElement;
+    const tr = td.parentElement;
+    const tbody = tr.parentElement;
+    tbody.removeChild(tr);
+
+    const result = person.filter(data => data.id !== Number(e.target.value));
+    person = result;
+    localStorage.setItem("person", JSON.stringify(person));
+  }
 });
